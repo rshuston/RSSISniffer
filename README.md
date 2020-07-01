0b26301c68875ea468989d1b0d0c53def9b59031
Robert Huston <robert.huston@pinpointdynamics.com>
2020-07-01 09:36:47 -0400

    Basic scanning. Works in foreground only.

---
+++ README.md
@@ -0,0 +1,8 @@
+#  RSSI Sniffer
+
+Make sure you add the following items to your `Info.plist` file:
+
+* Privacy - Bluetooth Always Usage Description (NSBluetoothAlwaysUsageDescription)  
+  String: This app needs Bluetooth to discover and interact with nearby devices
+* Privacy - Bluetooth Peripheral Usage Description (NSBluetoothPeripheralUsageDescription)
+  String: This app needs Bluetooth to discover and interact with nearby devices
--- RSSISniffer/ViewController.swift
+++ RSSISniffer/ViewController.swift
@@ -7,14 +7,79 @@
 //
 
 import UIKit
+import CoreBluetooth
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var StartStopButton: UIButton!
+    @IBOutlet weak var CentralStateLabel: UILabel!
+
+    var centralManager: CBCentralManager!
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        centralManager = CBCentralManager(delegate: self, queue: nil)
+    }
+    
+    @IBAction func doStartStop(_ sender: Any) {
+        if let buttonText = StartStopButton.title(for: .normal) {
+            switch buttonText {
+            case "Start":
+                startScanningForPeripherals()
+            case "Stop":
+                stopScanningForPeripherals()
+            default:
+                break
+            }
+        }
     }
 
+    func startScanningForPeripherals() {
+        centralManager.scanForPeripherals(withServices: nil, options: [CBCentralManagerScanOptionAllowDuplicatesKey: true])
+        StartStopButton.setTitle("Stop", for: .normal)
+    }
+
+    func stopScanningForPeripherals() {
+        centralManager.stopScan()
+        StartStopButton.setTitle("Start", for: .normal)
+    }
+
+}
+
+extension ViewController: CBCentralManagerDelegate {
+
+    func centralManagerDidUpdateState(_ central: CBCentralManager) {
+        let stateMsg: String
+        switch central.state {
+        case .unknown:
+            stateMsg = "central.state is .unknown"
+        case .resetting:
+            stateMsg = "central.state is .resetting"
+        case .unsupported:
+            stateMsg = "central.state is .unsupported"
+        case .unauthorized:
+            stateMsg = "central.state is .unauthorized"
+        case .poweredOff:
+            stateMsg = "central.state is .poweredOff"
+            stopScanningForPeripherals()
+        case .poweredOn:
+            stateMsg = "central.state is .poweredOn"
+            startScanningForPeripherals()
+        @unknown default:
+            stateMsg = "central.state default is \(central.state)"
+        }
+        print(stateMsg)
+        CentralStateLabel.text = stateMsg
+    }
+
+    func centralManager(_ central: CBCentralManager, didDiscover peripheral: CBPeripheral, advertisementData: [String : Any], rssi RSSI: NSNumber) {
+
+        let uuid = peripheral.identifier
+        let name = peripheral.name ?? "?"
+
+        print("peripheral: uuid = \(uuid), RSSI = \(RSSI.stringValue), name = \(name)")
+    }
 
 }
 
